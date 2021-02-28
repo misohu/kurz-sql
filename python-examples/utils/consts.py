5f4ba69ba7a8b53c0b8c5e5864fa0f40590a8dcc
Michal Hucko <michalhucko@Michals-MacBook-Pro.local>
2021-02-28 11:08:50 +0100

    Dev: adjust lecture 2 for python (consts)

--- python-examples/utils/consts.py
+++ python-examples/utils/consts.py
@@ -4,3 +4,5 @@ POSTGRES_HOST=os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_DATABASE=os.getenv("DATABASE", "school")
 POSTGRES_USER=os.getenv("POSTGRES_USER", "michal")
 POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD", "michal")
+
+POSTGRES_CONNECTION_STRING=os.getenv("POSTGRES_CONNECTION_STRING", "dbname=school user=michal password=michal host=localhost port=5432")
